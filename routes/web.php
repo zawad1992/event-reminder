a21618bacc7451ce0474f07edf701299e10462e0
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-24 00:27:43 +0600

    [Feat] Event Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Event Routes
+Route::get('/events', [EventsController::class, 'index'])->name('events.index');
