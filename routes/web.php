05ab850fbab87db3499d67018774635b55a0049e
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-24 04:03:18 +0600

    [Refactor] Route add

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ Route::get('/', function () {
 
 // Event Routes
 Route::get('/events', [EventsController::class, 'index'])->name('events.index');
+Route::get('/get-events', [EventsController::class, 'get_events'])->name('events.get_events');
+Route::get('/event_types', [EventsController::class, 'types'])->name('event_types.index');
+Route::post('/event_type/add', [EventsController::class, 'type_add'])->name('event_types.add');
+Route::put('/event_type/update/{id}', [EventsController::class, 'type_update'])->name('event_types.update');
+Route::delete('/event_type/delete/{id}', [EventsController::class, 'type_delete'])->name('event_types.delete');
