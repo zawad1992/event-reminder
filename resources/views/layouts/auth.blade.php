49b95f211548a14a6c7fa137718c7f6333bdb686
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-26 00:32:29 +0600

    [Refactor] Asset URL Change

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,10 +6,10 @@
   <title>{{ (!empty($title)) ? $title : env('APP_NAME') }} </title>
   <link rel="icon" href="{{ url('favicon.ico') }}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="{{ url('public/assets/plugins/fontawesome-free/css/all.min.css') }}">
-  <link rel="stylesheet" href="{{ url('public/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ url('public/assets/dist/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{ url('public/assets/css/common.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/common.css') }}">
 
 
   @stack('styles')
@@ -22,9 +22,9 @@
 <!-- /.login-box -->
 
 
-<script src="{{ url('public/assets/plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ url('public/assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ url('public/assets/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ url('assets/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('assets/dist/js/adminlte.min.js') }}"></script>
 @stack('scripts')
 
 </body>
