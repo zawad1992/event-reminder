1ae534556542f7a2a8a2f7f6986123e6b5696d2c
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-25 21:19:08 +0600

    [Refactor] Fix Home URL

--- resources/views/layouts/includes/maincontent.blade.php
+++ resources/views/layouts/includes/maincontent.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
             <li class="breadcrumb-item active">{!! (!empty($title_for_layout)) ? $title_for_layout : '&nbsp;' !!}</li>
           </ol>
         </div>
