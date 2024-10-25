119b59e1772bc9bc2586e90dd564685931266640
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-25 21:06:19 +0600

    [Refactor] Secure the Routes

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventsController;
+use App\Http\Controllers\HomeController;
 
+// Public routes (accessible to everyone)
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Authentication Routes
+Auth::routes();
 
-// Event Routes
+// Protected routes (only for authenticated users)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', [EventsController::class, 'calendar'])->name('events.calendar');
-Route::get('/events', [EventsController::class, 'calendar'])->name('events.calendar');
+    // Calendar Routes
+    Route::get('/events', [EventsController::class, 'calendar'])->name('events.calendar');
+    Route::get('/get_events', [EventsController::class, 'get_events'])->name('events.get_events');
 
-Route::get('/events/list', [EventsController::class, 'index'])->name('events.index');
+    // Event CRUD Routes
+    Route::get('/events/list', [EventsController::class, 'index'])->name('events.index');
+    Route::get('/event/add', [EventsController::class, 'event_add'])->name('events.add');
+    Route::post('/event/add', [EventsController::class, 'event_submit'])->name('events.add');
+    Route::put('/event/update/{id}', [EventsController::class, 'event_update'])->name('events.update');
+    Route::put('/event/complete/{id}', [EventsController::class, 'event_complete'])->name('events.complete');
+    Route::delete('/event/delete/{id}', [EventsController::class, 'event_delete'])->name('events.delete');
 
-Route::get('/event/add', [EventsController::class, 'event_add'])->name('events.add');
-Route::post('/event/add', [EventsController::class, 'event_submit'])->name('events.add');
-Route::put('/event/update/{id}', [EventsController::class, 'event_update'])->name('events.update');
-// events.upload
-Route::post('/event/upload', [EventsController::class, 'event_upload'])->name('events.upload');
+    // Event Upload Routes
+    Route::post('/event/upload', [EventsController::class, 'event_upload'])->name('events.upload');
+    Route::get('/event/sample-csv', [EventsController::class, 'downloadSampleCsv'])
+        ->name('events.sample-csv');
 
-Route::get('/event/sample-csv', [EventsController::class, 'downloadSampleCsv'])
-    ->name('events.sample-csv');
-
-Route::put('/event/complete/{id}', [EventsController::class, 'event_complete'])->name('events.complete');
-Route::delete('/event/delete/{id}', [EventsController::class, 'event_delete'])->name('events.delete');
-
-
-
-Route::get('/get_events', [EventsController::class, 'get_events'])->name('events.get_events');
-Route::get('/event_types', [EventsController::class, 'types'])->name('event_types.index');
-Route::post('/event_type/add', [EventsController::class, 'type_add'])->name('event_types.add');
-Route::put('/event_type/update/{id}', [EventsController::class, 'type_update'])->name('event_types.update');
-Route::delete('/event_type/delete/{id}', [EventsController::class, 'type_delete'])->name('event_types.delete');
+    // Event Types Routes
+    Route::get('/event_types', [EventsController::class, 'types'])->name('event_types.index');
+    Route::post('/event_type/add', [EventsController::class, 'type_add'])->name('event_types.add');
+    Route::put('/event_type/update/{id}', [EventsController::class, 'type_update'])->name('event_types.update');
+    Route::delete('/event_type/delete/{id}', [EventsController::class, 'type_delete'])->name('event_types.delete');
+});
