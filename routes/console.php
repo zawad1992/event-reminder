1781bfc85dc2179bbe13acc80c6541f4348d224b
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-26 13:51:34 +0600

    [Refactor] Mail Sending Schedule

--- routes/console.php
+++ routes/console.php
@@ -1,17 +1,23 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 
+// Every 30 minutes
 Schedule::command('events:send-reminders')
-    ->daily()
     ->name('Send Event Reminders')
-    ->description('Sends reminder emails for upcoming events');
+    ->description('Sends reminder emails for upcoming events')
+    ->onFailure(function () {
+        Log::error('Event reminder failed to send');
+    })    
+    ->everyThirtyMinutes()
+    ->emailOutputOnFailure(Env::get('MAIL_FROM_ADDRESS'));
