74755f94dc99d7078a009051a95fd4ac2e5022ef
ZAWADUL KAWUM <zawad1992@gmail.com>
2024-10-26 13:55:07 +0600

    [Feat] Test mail system

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Email</title>
+</head>
+<body>
+    <h1>Test Email</h1>
+    <p>This is a test email sent from Laravel application.</p>
+    <p>Time sent: {{ date('Y-m-d H:i:s') }}</p>
+</body>
+</html>
